afec8b2a733069c4df3d6210a14ee70605347a8a
Doris <doris070714@gmail.com>
2023-02-11 09:56:13 +0800

    Initial Commit

---
+++ SwiftUIAR/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIAR
+//
+//  Created by Doris Trakarskys on 2023/2/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIAR/SwiftUIARApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIARApp.swift
+//  SwiftUIAR
+//
+//  Created by Doris Trakarskys on 2023/2/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
